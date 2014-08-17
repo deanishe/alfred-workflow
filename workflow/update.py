5f3c9e1d8029bd8545d3a3d3993f399eaeb2dc55
Fabio Niephaus <fniephaus@users.noreply.github.com>
2014-08-17 16:15:12 -0400

    Separate update checks

--- workflow/update.py
+++ workflow/update.py
@@ -25,6 +25,7 @@ wf = Workflow()
 auto_update({
     'github_slug': 'username/reponame',  # GitHub slug
     'version': 'v1.0',  # Version number
+    'auto': False, # Start update process automatically
     'frequency': 7, # Optional
 })
 
@@ -47,7 +48,7 @@ from workflow import Workflow
 from background import is_running, run_in_background
 from web import get
 
-__all__ = ['auto_update']
+__all__ = ['update', 'update_available']
 
 wf = Workflow()
 log = wf.logger
@@ -56,19 +57,28 @@ DEFAULT_FREQUENCY = 7
 RELEASES_BASE = 'https://api.github.com/repos/%s/releases'
 
 
-def auto_update(config, force=False):
+def update(config, force=False):
     try:
         wf.settings['__update_github_slug'] = config['github_slug']
         wf.settings['__update_version'] = config['version']
     except KeyError:
         raise ValueError('Auto update settings incorrect')
+    if 'auto' in config:
+        wf.settings['__update_auto'] = config['auto']
     if 'frequency' in config:
         wf.settings['__update_frequency'] = config['frequency']
     if force:
-        wf.cache_data('auto_update', None)
-    if not is_running('auto_update'):
+        wf.cache_data('__update', None)
+    if not is_running('__update'):
         cmd = ['/usr/bin/python', wf.workflowfile('workflow/update.py')]
-        run_in_background('auto_update', cmd)
+        run_in_background('__update', cmd)
+
+def update_available():
+    update_data = cached_data('__update')
+    if (update_data is None or
+        'available' not in update_data):
+        return False
+    return update_data['available']
 
 def _download_workflow(github_url):
     filename = github_url.split("/")[-1]
@@ -114,22 +124,46 @@ def _get_latest_release(release_list):
     if len(release_list) < 1:
         raise RuntimeError('No release found')
     return release_list[0]
-def main(wf):
+
+def _update_available():
     github_slug = wf.settings['__update_github_slug']
     current_version = wf.settings['__update_version']
-    if wf.cached_data_fresh('auto_update', _frequency()):
-        return False
     release_list = get(_get_api_url(github_slug)).json()
     latest_release = _get_latest_release(release_list)
     latest_version = _extract_version(latest_release)
     if _is_latest(current_version, latest_version):
+        wf.cache_data('__update', {
+            'available': False
+        })
         return False
-    download_url = _extract_download_url(latest_release)
-    local_file = _download_workflow(download_url)
-    wf.cache_data('auto_update', True)
+    wf.cache_data('__update', {
+        'version': latest_version,
+        'download_url': _extract_download_url(latest_release),
+        'available': True
+    })
+    return True
+
+def _initiate_update():
+    _update_available()
+    update_data = cached_data('__update')
+    if (update_data is None or
+        'download_url' not in update_data):
+        return False
+    local_file = _download_workflow(update_data['download_url'])
+    wf.cache_data('__update', True)
     os.system('open "%s"' % local_file)
     wf.logger.debug('Update initiated')
     return True
+
+def main(wf):
+    if wf.cached_data_fresh('__update', _frequency()):
+        return False
+
+    if ('__update_auto' in wf.settings and 
+            wf.settings['__update_auto'] == True):
+        return _initiate_update()
+    else:
+        return _update_available()
 
 if __name__ == '__main__':  # pragma: no cover
     wf.run(main)

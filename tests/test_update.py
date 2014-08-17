ae3a380e59aad538420b0ebcb8daf8c9ce14a78e
Fabio Niephaus <fniephaus@users.noreply.github.com>
2014-08-16 20:12:01 -0400

    Move auto_update to update.py

--- tests/test_update.py
+++ tests/test_update.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 #
-# Copyright (c) 2014 Dean Jackson <deanishe@deanishe.net>, Fabio Niephaus <fabio.niephaus@gmail.com>
+# Copyright (c) 2014 Fabio Niephaus <fabio.niephaus@gmail.com>, Dean Jackson <deanishe@deanishe.net>
 #
 # MIT Licence. See http://opensource.org/licenses/MIT
 #
@@ -14,58 +14,78 @@ Test auto update capabilities
 from __future__ import print_function
 
 import unittest
+from pkg_resources import parse_version
 
 from workflow import Workflow
+import workflow.update as update
 
 
 class UpdateTests(unittest.TestCase):
 
-    def test_disabled(self):
-        wf = Workflow()
-        self.assertFalse(wf.auto_update())
-        wf.reset()
+    def setUp(self):
+        self.wf = Workflow()
 
-    def test_bad_github_slug(self):
-        wf = Workflow(default_settings={
-            'auto_update_github': 'fniephausalfred-pocket',
-            'auto_update_version': 'v0.0',
-        }, autoupdate=False)
-        self.assertFalse(wf.auto_update())
-        wf.reset()
+    def test_auto_update(self):
+        """Test auto update"""
+        self.assertRaises(ValueError, update.auto_update, {'frequency': 1})
+        slug = 'fniephaus/alfred-pocket'
+        version = 'v999.9'
+        frequency = 8
+        update.auto_update({
+            'github_slug': slug,  # GitHub slug
+            'version': version,  # Version number
+            'frequency': frequency, # Optional
+        }, force=True)
+        self.assertEquals(self.wf.settings['auto_update_github_slug'], slug)
+        self.assertEquals(self.wf.settings['auto_update_version'], version)
+        self.assertEquals(self.wf.settings['auto_update_frequency'], frequency)
 
-    def test_no_workflow_attachment(self):
-        wf = Workflow(default_settings={
-            'auto_update_github': 'deanishe/alfred-workflow',
-            'auto_update_version': 'v0.0',
-        }, autoupdate=False)
-        self.assertFalse(wf.auto_update())
-        wf.reset()
+    def test_download_workflow(self):
+        """Update: Bad attachment"""
+        self.assertRaises(RuntimeError, update._download_workflow, 'http://github.com/test.zip')
 
-    def test_no_releases(self):
-        wf = Workflow(default_settings={
-            'auto_update_github': 'django/django',
-            'auto_update_version': 'v0.0',
-        }, autoupdate=False)
-        self.assertFalse(wf.auto_update())
-        wf.reset()
+    def test_frequency(self):
+        """Update: Frequency"""
+        update.wf.settings['auto_update_frequency'] = None
+        self.assertEquals(update._frequency(), update.DEFAULT_FREQUENCY * 60 * 60 * 24)
+        update.wf.settings['auto_update_frequency'] = 14
+        self.assertEquals(update._frequency(), 14 * 60 * 60 * 24)
 
-    def test_up_to_date(self):
-        wf = Workflow(default_settings={
-            'auto_update_github': 'fniephaus/alfred-pocket',
-            'auto_update_version': 'v999.9',
-            'auto_update_frequency': 14,
-        })
-        self.assertFalse(wf.auto_update())
-        wf.reset()
+    def test_get_api_url(self):
+        """Update: Get API URL"""
+        url = update._get_api_url('fniephaus/alfred-workflow')
+        expected = 'https://api.github.com/repos/fniephaus/alfred-workflow/releases'
+        self.assertEquals(url, expected)
+        self.assertRaises(ValueError, update._get_api_url, 'fniephausalfred-workflow')
 
-    def test_update(self):
-        wf = Workflow(default_settings={
-            'auto_update_github': 'fniephaus/alfred-pocket',
-            'auto_update_version': 'v0.0',
-        }, autoupdate=False)
-        self.assertTrue(wf.auto_update())
-        self.assertFalse(wf.auto_update())
+    def test_extract_version(self):
+        """Update: Extract version"""
+        version = update._extract_version({'tag_name': 'v1.0'})
+        expected = parse_version('v1.0')
+        self.assertEquals(version, expected)
+        self.assertRaises(RuntimeError, update._extract_version, {})
 
+    def test_extract_download_url(self):
+        """Update: Extract download URL"""
+        url = update._extract_download_url({'assets': [{'browser_download_url': 'http://github.com/'}]})
+        expected = 'http://github.com/'
+        self.assertEquals(url, expected)
+        self.assertRaises(RuntimeError, update._extract_download_url, {})
+
+    def test_get_latest_release(self):
+        """Update: Extract download URL"""
+        release_list = update._get_latest_release([1, 2, 3])
+        self.assertEquals(release_list, 1)
+        self.assertRaises(RuntimeError, update._get_latest_release, [])
+
+    def test_main(self):
+        """Update: Main function"""
+        self.wf.settings['auto_update_version'] = 'v999.9'
+        self.assertFalse(update.main(self.wf))
+        self.wf.clear_cache()
+        self.wf.settings['auto_update_version'] = 'v0.0'
+        self.assertTrue(update.main(self.wf))
+        self.assertFalse(update.main(self.wf))
 
 if __name__ == '__main__':  # pragma: no cover
     unittest.main()
---
+++ workflow/update.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Copyright © 2014 Fabio Niephaus <fabio.niephaus@gmail.com>, Dean Jackson <deanishe@deanishe.net>
+#
+# MIT Licence. See http://opensource.org/licenses/MIT
+#
+# Created on 2014-08-16
+#
+
+"""
+Enables self-updating capabilities for a workflow. It regularly (7 days
+by default) fetches the latest releases from a given GitHub repository
+and then asks the user to replace the workflow if a newer version is
+available.
+
+This feature requires default settings to be set like this:
+
+
+from workflow import Workflow
+from workflow.update import auto_update
+...
+wf = Workflow()
+...
+auto_update({
+    'github_slug': 'username/reponame',  # GitHub slug
+    'version': 'v1.0',  # Version number
+    'frequency': 7, # Optional
+})
+
+:param config: Auto update configuration
+:type config: ``dict``
+:param force: Force an update check
+:type force: ``Boolean``
+:rtype: ``Boolean``
+
+"""
+
+from __future__ import print_function, unicode_literals
+
+import os
+import urllib
+import tempfile
+from pkg_resources import parse_version
+
+from workflow import Workflow
+from background import is_running, run_in_background
+from web import get
+
+__all__ = ['auto_update']
+
+wf = Workflow()
+log = wf.logger
+
+DEFAULT_FREQUENCY = 7
+RELEASES_BASE = 'https://api.github.com/repos/%s/releases'
+
+
+def auto_update(config, force=False):
+    try:
+        wf.settings['auto_update_github_slug'] = config['github_slug']
+        wf.settings['auto_update_version'] = config['version']
+    except KeyError:
+        raise ValueError('Auto update settings incorrect')
+    if 'frequency' in config:
+        wf.settings['auto_update_frequency'] = config['frequency']
+    if force:
+        wf.cache_data('auto_update', None)
+    if not is_running('auto_update'):
+        cmd = ['/usr/bin/python', wf.workflowfile('workflow/update.py')]
+        run_in_background('auto_update', cmd)
+
+def _download_workflow(github_url):
+    filename = github_url.split("/")[-1]
+    if (not github_url.endswith('.alfredworkflow') or
+            not filename.endswith('.alfredworkflow')):
+        raise RuntimeError('Attachment %s not a workflow' % filename)
+    download_url = get(github_url).url # resolve redirect
+    file_downloader = urllib.URLopener()
+    local_file = '%s/%s' % (tempfile.gettempdir(), filename)
+    file_downloader.retrieve(download_url, local_file)
+    return local_file
+
+def _frequency():
+    frequency = None
+    if 'auto_update_frequency' in wf.settings:
+        frequency = wf.settings['auto_update_frequency']
+    if isinstance(frequency, int):
+        return frequency * 86400
+    else:
+        return DEFAULT_FREQUENCY * 86400
+
+def _get_api_url(slug):
+    if len(slug.split('/')) != 2:
+        raise ValueError('Invalid GitHub slug : %s' % slug)
+    return RELEASES_BASE % slug
+
+def _extract_version(release):
+    if 'tag_name' not in release:
+        raise RuntimeError('No version found')
+    return parse_version(release['tag_name'])
+
+def _extract_download_url(release):
+    if ('assets' not in release or
+            len(release['assets']) != 1 or
+            'browser_download_url' not in release['assets'][0]):
+        raise RuntimeError('No attachment found')
+    return release['assets'][0]['browser_download_url']
+
+def _get_latest_release(release_list):
+    if len(release_list) < 1:
+        raise RuntimeError('No release found')
+    return release_list[0]
+
+def main(wf):
+    github_slug = wf.settings['auto_update_github_slug']
+    current_version = parse_version(wf.settings['auto_update_version'])
+    if wf.cached_data_fresh('auto_update', _frequency()):
+        return False
+    release_list = get(_get_api_url(github_slug)).json()
+    latest_release = _get_latest_release(release_list)
+    latest_version = _extract_version(latest_release)
+    if current_version >= latest_version:
+        return False
+    download_url = _extract_download_url(latest_release)
+    local_file = _download_workflow(download_url)
+    wf.cache_data('auto_update', True)
+    os.system('open "%s"' % local_file)
+    wf.logger.debug('Update initiated')
+    return True
+
+if __name__ == '__main__':  # pragma: no cover
+    wf.run(main)
+    

38ea964a0d74e5b8fe2bf53ceed8ae9dab643722
Dean Jackson <deanishe@deanishe.net>
2016-02-27 23:12:35 +0100

    Install Travis deps in a separate script

---
+++ extras/travis-install-deps.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Install Python dependencies for Travis-CI.org
+
+set -e
+
+log() {
+  echo "$@" > /dev/stderr
+}
+
+export CC=clang
+
+log "----------------- Test dependencies ----------------"
+pip install \
+  pytest \
+  coveralls \
+  pytest_cov \
+  pytest_httpbin \
+  pytest_localserver
+
+if [[ "$VERSION" =~ ^2.7.* ]]; then
+  log "----------------------- PyObc ----------------------"
+  pip install pyobjc-core
+  pip install pyobjc-framework-Cocoa
+fi
+
+log "---------------------- Done ------------------------"
